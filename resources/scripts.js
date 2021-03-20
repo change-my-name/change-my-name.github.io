5609d98a0ef5a7fcb41c82d3f1b9ac28cacc57d7
Spacener <spst7341@colorado.edu>
2021-03-20 12:38:12 -0600

    Another push

--- resources/scripts.js
+++ resources/scripts.js
@@ -8,18 +8,7 @@ var myFunction = function() {
     setTimeout(() => {
         document.getElementById('body').style.background === "#ffcce8";
     }, 1000);
-
-    if (document.getElementById('body').style.background === "#ccdbff") {
-        bgColor = "#ffcce8";
-    }
-    else {
-        bgColor = "#ccdbff";
-    }
-    document.getElementById('body').style.background = bgColor;
 };
-
-var timer = setInterval(myFunction, 1500); // call every 1000 milliseconds
-
 
 function loadPageData() {
     $.getJSON('page_data.json', function(data) {
