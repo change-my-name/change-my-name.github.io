ff1dc9178d8a062467d008e8c08b928b63608a66
Spacener <spst7341@colorado.edu>
2021-03-21 01:11:30 -0600

    Search Bar yeet

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,6 +1,6 @@
 
 
-var myFunction = function() {
+var colorChange = function() {
 
     if (document.getElementById('body').style.background === "#ccdbff") {
         bgColor = "#ffcce8";
@@ -11,7 +11,7 @@ var myFunction = function() {
     document.getElementById('body').style.background = bgColor;
 };
 
-var timer = setInterval(myFunction, 1500); // call every 1000 milliseconds
+var timer = setInterval(colorChange, 1500); // call every 1000 milliseconds
 
 
 function loadPageData() {
@@ -114,3 +114,28 @@ function GetURLParameter(sParam){
       }
     }
   }
+
+
+
+function searchFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchBar");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("theList");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
